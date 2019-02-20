5ff297ee1682a70398baf0502f60618168c995f6
aAntak <andriusk1997@gmail.com>
2019-02-20 17:08:02 +0200

    Add movement system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,61 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
+    [SerializeField] int health;
+    [SerializeField] int xSpeed;
+    [SerializeField] int ySpeed;
+    
+
+    Rigidbody2D rb;
+
+    [SerializeField] bool IsinitialWall;
+
 	void Start () {
-		
+        IsinitialWall = true;
+
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.velocity = new Vector3(rb.velocity.x, 2, 0);
+       
+        
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        SwitchPlatform();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    { 
+        rb.velocity = new Vector3(0, rb.velocity.y, 0);
+
+        IsinitialWall = !IsinitialWall;
+    }
+
+    void SwitchPlatform()
+    {       
+        float direction;
+        if (IsinitialWall) direction = 5;
+        else direction = -5;
+
+
+        if (Input.GetKeyDown("space"))
+        {
+
+
+            rb.velocity = new Vector3(direction*2.5f, rb.velocity.y, 0);
+
+
+            //Debug.Log(transform.position.x);
+            //Debug.Log(targetWall.x);
+            
+        }
+        //float distance = Mathf.Abs(transform.position.x - targetWall.x);
+        //Debug.Log(distance);
+        
+       
+
+    }
 }
